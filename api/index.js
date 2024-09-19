1d8169c4720407652bfed7f41ac77f43cf8d4a0d
Ealam Taher <ealozaher@gmail.com>
2024-09-19 11:46:46 +0300

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 mongoose
@@ -14,9 +15,10 @@ mongoose
   });
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
