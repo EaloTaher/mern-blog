306b3cafb5f1dbe1708061ada8b46a5ae3810170
Ealam Taher <ealozaher@gmail.com>
2024-09-25 12:03:41 +0300

    added updateUser api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -16,6 +17,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
